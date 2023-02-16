df5b28ce1472e315b2d2a7cadb46bb7303d2c593
PLJNS <me@pljns.com>
2023-02-16 02:41:36 -0500

    ReactPress working

--- nextjs/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    basePath: "/app",
-    assetPrefix: "/app/",
-    /* rest of the configuration */
-};
-
-module.exports = nextConfig;
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
@@ -7,17 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>{" "}
+        <Button as="a" variant="primary">
+          Button as link{" "}
+        </Button>{" "}
+        <Button as="a" variant="success">
+          Button as link{" "}
+        </Button>{" "}
+      </header>{" "}
     </div>
   );
 }
